3caaa99e656190123fe4645458647f5eb44f92ea
sikwan <yysskk99999@naver.com>
2024-02-04 21:48:39 +0900

    fetch Method

--- Sources/BoxOffices/Errors/APIError.swift
+++ Sources/BoxOffices/Errors/APIError.swift
@@ -9,4 +9,32 @@ import Foundation
 
 enum APIError : Error {
     case urlIsInvalid
+    case responseIsNotExpected
+    case requestIsInvalid(_ statusCode: Int)
+    case serverIsnotResponding(_ statusCode: Int)
+    case responseIsUnsuccessful(_ statusCode: Int)
+}
+
+extension APIError {
+    init?(httpResponse : HTTPURLResponse?) {
+        
+        guard let httpResponse else {
+            self = APIError.responseIsNotExpected// error
+            return
+        }
+        switch httpResponse.statusCode {
+      
+        case 200..<300:
+         return nil
+       // case 300..<400:
+        case 400..<500:
+            
+            self = APIError.requestIsInvalid(httpResponse.statusCode)
+        case 500..<600:
+            self = APIError.serverIsnotResponding(httpResponse.statusCode)
+        default:
+            self = APIError.responseIsUnsuccessful(httpResponse.statusCode)
+        }
+      
+    }
 }
--- Sources/BoxOffices/Network/APIClient.swift
+++ Sources/BoxOffices/Network/APIClient.swift
@@ -16,4 +16,25 @@ class APIClient {
     init(key: String) {
         self.key = key
     }
+    
+    func fetch<Response : Decodable>
+    (
+        path: String, // 요청 url 에서 base url 을 제외한 부분을 경로라고 생각 하면댐
+        // searchDailyBoxOfficeList.json
+        httpMethod: HTTPMethod,
+        queryItems : [URLQueryItem]? = nil
+    )async throws -> Response {
+        // URL 요청 생성
+        let urlRequest = try URLRequest(urlString: "\(baseURL)/\(path)", httpMethod: httpMethod, key: key, queryitems: queryItems)
+  // try를 생성하는 이유는 URLRequest에서 내가 생성자를 만들때 try를 만들었기때문임
+        
+        let (data,URLResponse) = try await URLSession.shared.data(for: urlRequest)
+        
+        if let error = APIError(httpResponse: URLResponse as? HTTPURLResponse) {
+            throw error
+        }
+        //제네릭으로 설정한 response로 data를 디코딩한다.
+        let output = try JSONDecoder().decode(Response.self, from: data)
+        return output
+    }
 }
