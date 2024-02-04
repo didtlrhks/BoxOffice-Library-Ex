90346c00a732bdee76223d0889b62bcf46148bc0
sikwan <yysskk99999@naver.com>
2024-02-04 20:27:28 +0900

    init 생성

---
+++ Sources/BoxOffices/Errors/APIError.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by 양시관 on 2/4/24.
+//
+
+import Foundation
+
+enum APIError : Error {
+    case urlIsInvalid
+}
--- Sources/BoxOffices/Extensions/URLRequest.BoxOffices.swift
+++ Sources/BoxOffices/Extensions/URLRequest.BoxOffices.swift
@@ -4,7 +4,7 @@
 //
 //  Created by 양시관 on 2/3/24.
 //
-
+//생성자 정의 하기
 import Foundation
 
 extension URLRequest {
@@ -16,8 +16,20 @@ extension URLRequest {
         
     ) throws{
         //urlComponents
-        guard let components = URLComponents(string: urlString) else{
-            throw// error 를 던질거임
+        guard var components = URLComponents(string: urlString) else{
+            throw APIError.urlIsInvalid// error 를 던질거임
         }
+        //쿼리 아이템
+        components.queryItems = [URLQueryItem(name: "key", value: key)]
+        if let queryitems {
+            components.queryItems?.append(contentsOf: queryitems)
+        }
+        // 유일엘 요청
+        guard let url = components.url else {
+            throw APIError.urlIsInvalid
+        }
+        self.init(url:url)
+        
+        self.httpMethod = httpMethod.capitalizedValue
     }
 }
