68b590ea35c80d9bb0021cd38d30e562918a888f
sikwan <yysskk99999@naver.com>
2024-02-10 18:05:11 +0900

    Document add

--- Sources/BoxOffices/BoxOffices.swift
+++ Sources/BoxOffices/BoxOffices.swift
@@ -9,18 +9,23 @@ import Foundation
 //fileprivate
 //private
 
-
+/// 박스오피스 관련 퍼블릭 인터페이스를 제공하는 메인 클래스
 public class BoxOffices {
    private let apiClient: APIClient
     
+    ///영화 진흥위원회에서 발급받은 API키
    public var apiKey: String {
         apiClient.key
     }
     
+    ///''BoxOffices/BoxOffices''
+    /// - Parameter key : 영화진흥위원회에서 발급받은 API 키
     public init(key : String) {
         self.apiClient = APIClient(key: key)
     }
     
+    ///박스오피스 일별 탑 10 영화를 가져옵니다.
+    /// - Returns: 성공할 시에는 '' Movie '' 타입의 영화 10개 , 실패할 시에는 에러를 제공하는 'Result' 객체
    public func fetchDailyTop10() async -> Result<[Movie], Error> {
         //어제꺼를 가져와야함
         guard let yesterday = Date().yesterday else {
